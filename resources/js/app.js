77121faf399355ba9143eb508142dd81146da8d3
Kyohey Sekiya <Kyohey.sekiya@gmail.com>
2020-12-21 12:33:47 +0900

    コンポーネントの表示

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
+// ルーティングの定義をインポートする
+import router from './router'
+// ルートコンポーネントをインポートする
+import App from './App.vue'
 
 new Vue({
   el: '#app',
-  template: '<h1>Hello world</h1>'
+  // ルーティングの定義を読み込む
+  router,
+  // ルートコンポーネントの使用を宣言する
+  components: { App },
+  // ルートコンポーネントを描画する
+  template: '<App />'
 })

3041d6177c8c39e96675a807c2a0520bc202629d
Kyohey Sekiya <Kyohey.sekiya@gmail.com>
2020-12-21 13:59:53 +0900

    ログインAPI テストコードと動作テスト

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+// 追加
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,11 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    // メソッド追加
+    protected function authenticated(Request $request, $user)
+    {
+      return $user;
     }
 }
